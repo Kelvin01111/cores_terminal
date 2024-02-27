ac4a0f6e5c94c4312bc51840e4be1e8d3fa07b16
Kelvin R Alcântara <127677144+Kelvin01111@users.noreply.github.com>
2024-02-27 09:24:19 -0300

    Add files via upload

---
+++ cores_terminal.py
@@ -0,0 +1,12 @@
+
+print("\033[1;31;43mOlá Mundo!")
+
+#STYLE           TEXT                BACK
+# 0 NONE         30 BRANCO            40
+# 1 BOLD         31 VERMELHO          41
+# 4 UNDERLINE    32 VERDE             42
+# 7 NEGATIVE     33 AMARELO           43
+#                34 AZUL ESCURO       44
+#                35 ROXO              45
+#                36 AZUL MARINHO      46
+#                37 CINZA             47
